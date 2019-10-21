608dd2f30350f902268003d41075678655a266ec
Chris <chris.ruegger@gmail.com>
2019-10-21 03:19:43 -0400

    Added join-cluster.sh

---
+++ bin/join-cluster.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubeadm join 192.168.254.40:6443 --token 3n30ja.6povejxoiwwup3ko \
+    --discovery-token-ca-cert-hash sha256:2d9f6a5884413d6f397670a39afb7359f7fd554f918fa1613cf0b48ebdbf05df
